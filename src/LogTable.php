f637e39c026805fcf46825999a669b70b86a660a
achyutkneupane <achyutkneupane@gmail.com>
2025-10-03 21:11:52 +0545

    fix: updates badge callbacks to return nullable integer values

--- src/LogTable.php
+++ src/LogTable.php
@@ -148,14 +148,14 @@ final class LogTable extends Page implements HasTable
     {
         $all_logs = [
             null => Tab::make('All Logs')
-                ->badge(fn (): int => Log::query()->count()),
+                ->badge(fn (): ?int => Log::query()->count() ?: null),
         ];
 
         $tabs = collect(LogLevel::cases())
             ->mapWithKeys(fn (LogLevel $level): array => [
                 $level->value => Tab::make($level->getLabel())
                     ->badge(
-                        fn (): int => Log::query()->where('log_level', $level)->count()
+                        fn (): ?int => Log::query()->where('log_level', $level)->count() ?: null,
                     )
                     ->badgeColor($level->getColor()),
             ])->toArray();
