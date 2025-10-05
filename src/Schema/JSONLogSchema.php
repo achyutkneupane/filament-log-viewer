d85a246e13cd546b1c8acf581a183c82c7b1a6d4
achyutkneupane <achyutkneupane@gmail.com>
2025-10-06 02:33:46 +0545

    feat: adds copyable and theme support for context in JSON log schema

--- src/Schema/JSONLogSchema.php
+++ src/Schema/JSONLogSchema.php
@@ -8,6 +8,7 @@ use Exception;
 use Filament\Infolists\Components\CodeEntry;
 use Filament\Schemas\Schema;
 use Phiki\Grammar\Grammar;
+use Phiki\Theme\Theme;
 
 final class JSONLogSchema
 {
@@ -20,6 +21,10 @@ final class JSONLogSchema
             ->key('json-log')
             ->components([
                 CodeEntry::make('context')
+                    ->copyable()
+                    ->copyMessage('Copied to Clipboard')
+                    ->lightTheme(Theme::CatppuccinMocha)
+                    ->darkTheme(Theme::CatppuccinMocha)
                     ->grammar(Grammar::Json),
             ]);
     }
