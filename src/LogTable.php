0f336a0ea65189feb60bb81d99211cb5400a84fd
achyutkneupane <achyutkneupane@gmail.com>
2025-11-29 00:33:54 +0545

    refactor: removes leading slash from navigation URL

--- src/LogTable.php
+++ src/LogTable.php
@@ -57,7 +57,10 @@ final class LogTable extends Page implements HasTable
     /** @throws Exception */
     public static function getSlug(): string
     {
-        return self::getPlugin()->getNavigationUrl();
+        return ltrim(
+            self::getPlugin()->getNavigationUrl(),
+            '/'
+        );
     }
 
     /** @throws Exception */
