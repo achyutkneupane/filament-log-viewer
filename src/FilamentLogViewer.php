dd218c5034aa0229e4fdbdfc15b5c3fd80ab9f6e
Achyut Neupane <30431426+achyutkneupane@users.noreply.github.com>
2026-01-30 22:23:43 +0545

    Update navigation functions to include return type

--- src/FilamentLogViewer.php
+++ src/FilamentLogViewer.php
@@ -30,13 +30,13 @@ final class FilamentLogViewer implements Plugin
         if ($navigationGroup) {
             $plugin->navigationGroup($navigationGroup);
         } else {
-            $plugin->navigationGroup(fn () => __('filament-log-viewer::log.navigation.group'));
+            $plugin->navigationGroup(fn (): string|array|null => __('filament-log-viewer::log.navigation.group'));
         }
 
         if ($navigationLabel) {
             $plugin->navigationLabel($navigationLabel);
         } else {
-            $plugin->navigationLabel(fn () => __('filament-log-viewer::log.navigation.label'));
+            $plugin->navigationLabel(fn (): string|array|null => __('filament-log-viewer::log.navigation.label'));
         }
 
         return $plugin;
