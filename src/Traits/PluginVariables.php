c767207cd5ba88870542c2a7cbf5954437b32486
jensdenijs <jens@solicon.be>
2026-03-13 14:20:33 +0100

    Add configurable registerNavigation option

--- src/Traits/PluginVariables.php
+++ src/Traits/PluginVariables.php
@@ -26,6 +26,8 @@ trait PluginVariables
 
     public string|null|Closure $pollingTime = null;
 
+    public bool|null|Closure $registerNavigation = true;
+
     public function isAuthorized(): bool
     {
         return (bool) $this->evaluate($this->authorized);
@@ -65,5 +67,11 @@ trait PluginVariables
     {
         /** @var string|null */
         return $this->evaluate($this->pollingTime);
+    }
+
+    public function shouldRegisterNavigation(): bool
+    {
+        /** @var bool */
+        return (bool) $this->evaluate($this->registerNavigation);
     }
 }
